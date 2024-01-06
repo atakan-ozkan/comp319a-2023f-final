7b1c2c729c464e90daca0e224fa3368e3415a338
Atakan Özkan <ozkanatakann@gmail.com>
2024-01-06 17:13:52 +0300

    add detail screen for each notes and functinality

--- LifeCanvas/app/src/main/java/com/example/lifecanvas/model/NoteModel.kt
+++ LifeCanvas/app/src/main/java/com/example/lifecanvas/model/NoteModel.kt
@@ -9,7 +9,7 @@ data class NoteModel(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val type: String, // "text", "voice", or "image"
     val title: String,
-    val content: String, // Text content or file path/URI for voice and image
+    var content: String, // Text content or file path/URI for voice and image
     val isPublic: Boolean, // true for public, false for private
     val filePath: String? = null, // File path for images/voice recordings, null for text notes
     val createdDate: Date,
